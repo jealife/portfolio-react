77b45271272e69da81a66c404eece8a58285c7ef
JEaLiFe <jealife.pictures@gmail.com>
2023-08-19 15:34:51 +0200

    Changes to be committed: 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Side from "./components/content/Side";
+import Main from "./components/content/Main";
 import Hero from "./components/hero/Hero";
 
 
@@ -8,26 +8,7 @@ function App() {
     <div className="App">
       <main className="main">
           <Hero/>
-          {/* <Main/> */}
-          <section  id='grid-main'>
-           <aside>
-                <Side />
-           </aside>
-            <section>
-                <div className='about-section' id="about">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur quod temporibus quos natus ullam, amet doloremque eius nisi repellat cumque distinctio nostrum libero fugiat possimus, adipisci nam recusandae animi, quia architecto obcaecati. Vero sequi accusamus autem ipsam non, voluptates quod earum laboriosam sapiente incidunt, quisquam eum dolore voluptate mollitia temporibus!
-                </div>
-                <div className='services-section' id="services">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur quod temporibus quos natus ullam, amet doloremque eius nisi repellat cumque distinctio nostrum libero fugiat possimus, adipisci nam recusandae animi, quia architecto obcaecati. Vero sequi accusamus autem ipsam non, voluptates quod earum laboriosam sapiente incidunt, quisquam eum dolore voluptate mollitia temporibus!
-                </div>
-                <div className='work-section' id="work">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur quod temporibus quos natus ullam, amet doloremque eius nisi repellat cumque distinctio nostrum libero fugiat possimus, adipisci nam recusandae animi, quia architecto obcaecati. Vero sequi accusamus autem ipsam non, voluptates quod earum laboriosam sapiente incidunt, quisquam eum dolore voluptate mollitia temporibus!
-                </div>
-                <div className='contact-section' id="contact">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur quod temporibus quos natus ullam, amet doloremque eius nisi repellat cumque distinctio nostrum libero fugiat possimus, adipisci nam recusandae animi, quia architecto obcaecati. Vero sequi accusamus autem ipsam non, voluptates quod earum laboriosam sapiente incidunt, quisquam eum dolore voluptate mollitia temporibus!
-                </div>
-            </section>
-        </section>
+          <Main/>
       </main>
     </div>
   );
