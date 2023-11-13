bccadfe1cdbecd3d62f99750fc8872c45a29eccd
JEaLiFe Jeaguy <jeanguylanememiaghe@gmail.com>
2023-11-13 17:03:06 +0000

    Changes to be committed: 	modified:   src/components/content/main.css 	modified:   src/components/services/Services.jsx 	modified:   src/index.css

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,12 +3,70 @@ import React from 'react'
 export default function Services() {
     return (
         <div>
-            <div className="service w-full">
-                <h1 className="font-bold text-gray-800 text-left">
+            <div className="service w-full flex flex-col gap-5 pt-24">
+                <h2 className="font-bold text-gray-800 text-left text-3xl">
                     Services
-                </h1>
-            </div>
+                </h2>
+                <div className="services-cards grid lg:grid-cols-3 grid-cols-1 gap-3">
 
+                    <div className="card-service bg-[#111928] p-6 rounded-md  flex flex-col gap-2">
+
+                        <div className="icon">
+                            <box-icon name='code-alt' size='md' color="#ff6d00"></box-icon>
+                        </div>
+                        <div className="content-card flex flex-col gap-3 mt-5">
+
+                            <div className="title">
+                                <h3 className='text-gray-200 text-xl font-semibold'>Developpment web</h3>
+                            </div>
+                            <div className="card-description">
+                                <p className='text-gray-400 text-lg font-light'>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis, inventore.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="card-service bg-[#111928] p-6 rounded-md  flex flex-col gap-2">
+
+                        <div className="icon">
+                            <box-icon type='solid' name='camera' size='md' color="#ff6d00"></box-icon>
+                        </div>
+                        <div className="content-card flex flex-col gap-3 mt-5">
+
+                            <div className="title">
+                                <h3 className='text-gray-200 text-xl font-semibold'>Photographie</h3>
+                            </div>
+                            <div className="card-description">
+                                <p className='text-gray-400 text-lg font-light'>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis, inventore.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="card-service bg-[#111928] p-6 rounded-md  flex flex-col gap-2">
+
+                        <div className="icon">
+                            <box-icon name='vector' type='solid' color="#ff6d00" size="md" ></box-icon>
+                        </div>
+                        <div className="content-card flex flex-col gap-3 mt-5">
+
+                            <div className="title">
+                                <h3 className='text-gray-200 text-xl font-semibold'>Conception graphiques</h3>
+                            </div>
+                            <div className="card-description">
+                                <p className='text-gray-400 text-lg font-light'>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis, inventore.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
         </div>
     )
 }
