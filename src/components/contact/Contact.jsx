39f9e79b0fe16a45e72a1e0144d067f6b193c558
JEaLiFe Jeaguy <jeanguylanememiaghe@gmail.com>
2023-11-13 17:19:31 +0000

    Changes to be committed: 	modified:   src/components/contact/Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div className='bg-[#111928]'>
+    <div className='bg-[#111928] w-full h-full p-24 rounded-lg'>
         <h1>Contact</h1>
     </div>
   )
